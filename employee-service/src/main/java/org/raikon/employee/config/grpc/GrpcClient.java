839979c892baed9cf2cc11e73ce603fb4cf211e0
raikon55 <edufelopes@gmail.com>
2020-08-30 01:07:12 -0300

    Fix gRPC configuration

--- employee-service/src/main/java/org/raikon/employee/config/grpc/GrpcClient.java
+++ employee-service/src/main/java/org/raikon/employee/config/grpc/GrpcClient.java
@@ -1,27 +1,30 @@
 package org.raikon.employee.config.grpc;
 
+import com.google.common.util.concurrent.ListenableFuture;
+import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.raikon.grpc.address.AddressControllerGrpc;
 import org.raikon.grpc.address.AddressGrpcRequest;
+import org.raikon.grpc.address.AddressGrpcResponse;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GrpcClient {
 
-    public void clientRequest() {
-        final var managedChannel = ManagedChannelBuilder.forAddress("localhost", 8087)
+    public AddressGrpcResponse clientRequest() {
+        final ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 8087)
                 .usePlaintext()
                 .build();
 
-        final var stub =
-                AddressControllerGrpc.newBlockingStub(managedChannel);
-
-        final var addressGrpcRequest = AddressGrpcRequest.newBuilder()
+        final AddressGrpcRequest addressGrpcRequest = AddressGrpcRequest.newBuilder()
                 .setNumber(1)
                 .setZipcode("31995000")
                 .build();
-        final var addressGrpcResponse = stub.create(addressGrpcRequest);
-        System.out.println(addressGrpcResponse);
+
+        final AddressGrpcResponse addressGrpcResponse =
+                AddressControllerGrpc.newBlockingStub(managedChannel).create(addressGrpcRequest);
         managedChannel.shutdown();
+
+        return addressGrpcResponse;
     }
 }
--- employee-service/src/main/java/org/raikon/employee/modules/address/service/impl/AddressProtoServiceImpl.java
+++ employee-service/src/main/java/org/raikon/employee/modules/address/service/impl/AddressProtoServiceImpl.java
@@ -1,6 +1,7 @@
 package org.raikon.employee.modules.address.service.impl;
 
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 import org.raikon.employee.dal.model.Address;
 import org.raikon.employee.modules.address.mapper.AddressMapper;
 import org.raikon.employee.modules.address.service.AddressService;
@@ -8,9 +9,8 @@ import org.raikon.grpc.address.AddressControllerGrpc;
 import org.raikon.grpc.address.AddressGrpcRequest;
 import org.raikon.grpc.address.AddressGrpcResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
+@GrpcService
 public class AddressProtoServiceImpl extends AddressControllerGrpc.AddressControllerImplBase {
 
     @Autowired
@@ -18,8 +18,9 @@ public class AddressProtoServiceImpl extends AddressControllerGrpc.AddressContro
 
     @Override
     public void create(AddressGrpcRequest request, StreamObserver<AddressGrpcResponse> responseObserver) {
-        final var address = this.addressService.create(AddressMapper.ofGrpc(request));
-        responseObserver.onNext(AddressMapper.toGrpcResponse(address));
+        final Address address = this.addressService.create(AddressMapper.ofGrpc(request));
+        final AddressGrpcResponse addressGrpcResponse = AddressMapper.toGrpcResponse(address);
+        responseObserver.onNext(addressGrpcResponse);
         responseObserver.onCompleted();
     }
 }
