2b5d6cb06a54ea3ee1875826aedb00ce58b0cf0e
raikon55 <edufelopes@gmail.com>
2020-09-01 21:42:37 -0300

    Add to be a discoverable client on Eureka

--- employee-service/src/main/java/org/raikon/employee/EmployeeApplication.java
+++ employee-service/src/main/java/org/raikon/employee/EmployeeApplication.java
@@ -2,9 +2,11 @@ package org.raikon.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = "org.raikon")
 public class EmployeeApplication {
 
